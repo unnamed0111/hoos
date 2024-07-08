cb0d2dbbdceb9f8abca5de4ae8f498db58c10140
unnamed <falconkick31@naver.com>
2024-07-09 00:46:38 +0900

    implementation window event function

--- Hoos/src/Hoos/Application.h
+++ Hoos/src/Hoos/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Hoos/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Hoos {
@@ -12,7 +14,11 @@ namespace Hoos {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
